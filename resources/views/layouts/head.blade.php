f4aa6b27bfb89c9bd6d6ec9a060ddc20205cdc97
Anwar Barakat <brkatanwar0@gmail.com>
2022-09-17 10:49:02 +0300

    merge the template

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,11 @@
+<link rel="icon" href="{{ asset('images/favicon/favicon.ico') }}" type="image/x-icon" />
+<link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" media="screen">
+<link href="{{ asset('assets/css/chblue.css') }}" rel="stylesheet" media="screen">
+<link href="{{ asset('assets/css/theme-responsive.css') }}" rel="stylesheet" media="screen">
+<link href="{{ asset('assets/css/dtb/jquery.dataTables.min.css') }}" rel="stylesheet" media="screen">
+<link href="{{ asset('assets/css/select2.min.css') }}" rel="stylesheet" media="screen">
+<link href="{{ asset('assets/css/toastr.min.css') }}" rel="stylesheet" media="screen">
+<script type="text/javascript" src="{{ asset('assets/js/jquery.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/js/jquery-ui.1.10.4.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/js/toastr.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/js/modernizr.js') }}"></script>
