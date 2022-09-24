3940ae5d3a7d48f515662addeec702348c0da522
Anwar Barakat <brkatanwar0@gmail.com>
2022-09-24 23:01:51 +0300

    show appline services in home page

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -12,10 +12,15 @@ class HomeComponent extends Component
     {
         $categories         = ServiceCategory::inRandomOrder()->take(18)->get();
         $featuredServices   = Service::where('featured', '1')->inRandomOrder()->take(8)->get();
+        $serviceCategories  = ServiceCategory::inRandomOrder()->take(8)->get();
+        $sid                = collect(ServiceCategory::inRandomOrder()->take(5)->get()->modelKeys());
+        $aservices          = Service::whereIn('service_category_id', $sid)->inRandomOrder()->take(8)->get();
 
         return view('livewire.home-component', [
             'categories'            => $categories,
             'featuredServices'      => $featuredServices,
+            'serviceCategories'     => $serviceCategories,
+            'aservices'             => $aservices
         ])->layout('layouts.master');
     }
 }
